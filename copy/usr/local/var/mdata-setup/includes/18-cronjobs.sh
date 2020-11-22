860711509e5bb58be991f7e96c3a711f8f4ff1f9
jfqd <jfqd@blun.org>
2020-11-22 21:59:26 +0100

    only add it once

--- copy/usr/local/var/mdata-setup/includes/18-cronjobs.sh
+++ copy/usr/local/var/mdata-setup/includes/18-cronjobs.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+if [[ `grep systemd_health_check /etc/cron.d/systemd_health_check |wc -l` -eq 0 ]]; then
+
 cat >> /etc/cron.d/systemd_health_check << EOF
 MAILTO=root
 #
-*/2 * * * *     root   /usr/local/bin/systemd_health_check
+# */2 * * * *     root   /usr/local/bin/systemd_health_check
 # END
 EOF
+
+fi
